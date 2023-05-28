affc77d2a0830e4a77f0616038a8c36eccc07780
HBrinkman <HenryBrinkman@gmail.com>
2023-05-28 08:23:33 +0000

    First steps

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,69 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
+import { WebContainer } from '@webcontainer/api';
+import { files } from './files';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+/** @type {import('@webcontainer/api').WebContainer}  */
+let webcontainerInstance: WebContainer;
+
+document.querySelector('#app')!.innerHTML = `
+  <div class="container">
+    <div class="editor">
+      <textarea>I am a textarea</textarea>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <div class="preview">
+      <iframe src="loading.html"></iframe>
+    </div>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+/** @type {HTMLIFrameElement | null} */
+const iframeEl = document.querySelector('iframe');
+
+/** @type {HTMLTextAreaElement | null} */
+const textareaEl = document.querySelector('textarea');
+
+window.addEventListener('load', async () => {
+  textareaEl!.value = files['index.ts'].file.contents;
+  // Call only once
+  console.log('Zie ik dit hier?');
+  WebContainer.boot().then(() => {
+    alert('Boot succeeded');
+  })
+  .catch((err) => {
+    alert(err);
+  });
+  webcontainerInstance = await WebContainer.boot();
+  await webcontainerInstance.mount(files);
+ 
+  const exitCode = await installDependencies();
+  if (exitCode !== 0) {
+    alert('Error');
+    throw new Error('Installation failed');
+  };
+
+  startDevServer();
+});
+
+async function installDependencies() {
+  // Install dependencies
+  const installProcess = await webcontainerInstance.spawn('npm', ['install']);
+  // Wait for install command to exit
+
+  installProcess.output.pipeTo(new WritableStream({
+    write(data) {
+      console.log(data);
+    }
+  }));
+
+  return installProcess.exit;
+}
+
+async function startDevServer() {
+  // Run `npm run start` to start the Express app
+  await webcontainerInstance.spawn('npm', ['run', 'start']);
+
+  // Wait for `server-ready` event
+  webcontainerInstance.on('server-ready', (port: any, url: string) => {
+    iframeEl!.src = url;
+  });
+}
