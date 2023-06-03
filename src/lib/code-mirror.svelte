d61c423f99ab093b710188296efd297827c91bd3
Henry Brinkman <Henry.Brinkman@topicus.nl>
2023-06-03 10:51:50 +0200

    Added code mirror

---
+++ src/lib/code-mirror.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	//import { EditorState } from '@codemirror/state';
+	//import { EditorView, keymap } from '@codemirror/view';
+	//import { defaultKeymap } from '@codemirror/commands';
+
+	//let startState = EditorState.create({
+//		doc: 'Hello World',
+//		extensions: [keymap.of(defaultKeymap)]
+//	});
+
+//	let view = new EditorView({
+//		state: startState,
+//		parent: document.body
+//	});
+//	console.log(view);
+</script>
+
+Hallo dit is een test
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+<header>
+  Header hier
+</header>
+<slot />
